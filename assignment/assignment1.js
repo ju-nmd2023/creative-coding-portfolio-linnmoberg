6f63bd7b4f2a52fdcec20112834763f1c9d27cad
linnmoberg <linn1.moberg@gmail.com>
2025-09-12 18:28:31 +0200

    first draft

---
+++ assignment/assignment1.js
@@ -0,0 +1,49 @@
+
+const gap = 20; 
+const numLines = 20;
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(255, 255, 255);
+ // noFill();
+  stroke(0, 0, 0);
+  strokeWeight(1);
+
+
+  let y = 80;
+ 
+  //for the horizontal lines
+  for (let i = 0; i < numLines; i++) {
+    let xStart = random(50, 150);
+    let xEnd = random(width - 150, width - 50);
+    line(xStart, y, xEnd, y);
+
+    //for the random smaller lines
+    let numDiagonals = int(random(1, 3));
+
+    for (let k = 0; k < numDiagonals; k++) {
+      let x = random(90, width - 90);
+      let len = random(30, 120);
+      let angle = random(-PI/4, PI/4);
+      //line(x, y, x + len * cos(angle), y + len * sin(angle));
+
+      push();
+      translate(x, y);
+      rotate(angle);
+      line(0, 0, len, 0);
+      pop();
+      
+      }
+    
+     y += gap;
+    
+    }
+    
+
+    noLoop();
+  }
+
+ 
